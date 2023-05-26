2c77bb0f5351e84cd6dea40d3ab2c9aa05569472
data <data.shkubuliani@redberry.ge>
2023-05-26 19:52:32 +0400

    feat(migration): update users table columns

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -12,12 +12,16 @@ return new class() extends Migration {
 	{
 		Schema::create('users', function (Blueprint $table) {
 			$table->id();
-			$table->string('name');
+			$table->string('username')->unique();
 			$table->string('email')->unique();
+			$table->string('email_verification_token')->unique()->nullable();
 			$table->timestamp('email_verified_at')->nullable();
-			$table->string('password');
+			$table->string('password')->nullable();
+			$table->string('google_id')->unique()->nullable();
 			$table->rememberToken();
 			$table->timestamps();
+
+			$table->index('email_verification_token');
 		});
 	}
 
