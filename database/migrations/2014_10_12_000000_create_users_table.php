02d2aaa18c28cb00c1d64140d14b01352ee60783
data <data.shkubuliani@redberry.ge>
2023-05-28 14:55:26 +0400

    feat: add profile picture column in users table and update user factory

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -17,6 +17,7 @@ return new class() extends Migration {
 			$table->string('email_verification_token')->unique()->nullable();
 			$table->timestamp('email_verified_at')->nullable();
 			$table->string('password')->nullable();
+			$table->string('profile_picture')->nullable();
 			$table->string('google_id')->unique()->nullable();
 			$table->rememberToken();
 			$table->timestamps();
