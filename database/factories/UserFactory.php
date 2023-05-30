41734c6adaa43015cbf95cf039f035a6d4f2599e
data <data.shkubuliani@redberry.ge>
2023-05-30 21:40:30 +0400

    chore: update password column in user factory

--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -30,7 +30,7 @@ class UserFactory extends Factory
 			'email'                    => fake()->unique()->safeEmail(),
 			'email_verification_token' => Str::uuid()->toString(),
 			'email_verified_at'        => now(),
-			'password'                 => '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi', // password
+			'password'                 => 'password', // The password will be automatically hashed via the users setPasswordAttribute method
 			'profile_picture'          => 'pictures/' . basename($image),
 			'remember_token'           => Str::random(10),
 		];
