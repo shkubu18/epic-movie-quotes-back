8a4a7b0724f234cca1e81a5e382fb14df7303f56
data <data.shkubuliani@redberry.ge>
2023-06-04 23:25:07 +0400

    feat: add MovieGenre model with its migration and factory

---
+++ database/migrations/2023_06_02_122246_create_movie_genres_table.php
@@ -0,0 +1,28 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration {
+	/**
+	 * Run the migrations.
+	 */
+	public function up(): void
+	{
+		Schema::create('movie_genres', function (Blueprint $table) {
+			$table->id();
+			$table->smallInteger('genre_id')->unsigned();
+			$table->smallInteger('movie_id')->unsigned();
+			$table->timestamps();
+		});
+	}
+
+	/**
+	 * Reverse the migrations.
+	 */
+	public function down(): void
+	{
+		Schema::dropIfExists('movie_genres');
+	}
+};
