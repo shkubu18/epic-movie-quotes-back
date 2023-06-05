80f3dfb978490e56af5a84403e3942ce9554b575
data <data.shkubuliani@redberry.ge>
2023-06-05 22:50:30 +0400

    feat: add hasmany relationship in Movie model for quotes

--- app/Models/Movie.php
+++ app/Models/Movie.php
@@ -5,6 +5,7 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsToMany;
+use Illuminate\Database\Eloquent\Relations\HasMany;
 use Spatie\Translatable\HasTranslations;
 
 class Movie extends Model
@@ -23,5 +24,10 @@ class Movie extends Model
 	public function genres(): BelongsToMany
 	{
 		return $this->belongsToMany(Genre::class, 'movie_genres', 'movie_id', 'genre_id');
+	}
+
+	public function quotes(): HasMany
+	{
+		return $this->hasMany(Quote::class);
 	}
 }
