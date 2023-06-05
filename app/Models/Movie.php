aae1019076b8125a9f5ccf56102f3a30575be68a
data <data.shkubuliani@redberry.ge>
2023-06-05 23:33:10 +0400

    fix(movies): update relation

--- app/Models/Movie.php
+++ app/Models/Movie.php
@@ -4,6 +4,7 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
 use Illuminate\Database\Eloquent\Relations\BelongsToMany;
 use Illuminate\Database\Eloquent\Relations\HasMany;
 use Spatie\Translatable\HasTranslations;
@@ -16,9 +17,9 @@ class Movie extends Model
 
 	public $translatable = ['name', 'director', 'description'];
 
-	public function user(): BelongsToMany
+	public function user(): BelongsTo
 	{
-		return $this->belongsToMany(User::class);
+		return $this->belongsTo(User::class);
 	}
 
 	public function genres(): BelongsToMany
