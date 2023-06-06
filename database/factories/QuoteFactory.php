94bddacbc2f8660c19c6cf1b9b9b5fc162dec085
data <data.shkubuliani@redberry.ge>
2023-06-07 00:36:21 +0400

    feat: add quotes factory and migration

--- database/factories/QuoteFactory.php
+++ database/factories/QuoteFactory.php
@@ -3,21 +3,34 @@
 namespace Database\Factories;
 
 use Illuminate\Database\Eloquent\Factories\Factory;
+use Illuminate\Support\Facades\File;
 
 /**
  * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Quote>
  */
 class QuoteFactory extends Factory
 {
-    /**
-     * Define the model's default state.
-     *
-     * @return array<string, mixed>
-     */
-    public function definition(): array
-    {
-        return [
-            //
-        ];
-    }
+	/**
+	 * Define the model's default state.
+	 *
+	 * @return array<string, mixed>
+	 */
+	public function definition(): array
+	{
+		// Check if the directory exists and create it if it does not.
+		if (!File::exists('public/storage/quotes/pictures')) {
+			File::makeDirectory('public/storage/quotes/pictures', $mode = 0755, true, true);
+		}
+
+		$image = \Faker\Factory::create()->image('public/storage/quotes/pictures');
+
+		return [
+			'name' => [
+				'en' => \Faker\Factory::create('en_US')->realText(30),
+				'ka' => \Faker\Factory::create('ka_GE')->realText(30),
+			],
+			'movie_id'      => fake()->numberBetween(1, 10),
+			'picture'       => 'quotes/pictures/' . basename($image),
+		];
+	}
 }
--- database/migrations/2023_06_05_183833_create_quotes_table.php
+++ database/migrations/2023_06_05_183833_create_quotes_table.php
@@ -4,24 +4,26 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-return new class extends Migration
-{
-    /**
-     * Run the migrations.
-     */
-    public function up(): void
-    {
-        Schema::create('quotes', function (Blueprint $table) {
-            $table->id();
-            $table->timestamps();
-        });
-    }
+return new class extends Migration {
+	/**
+	 * Run the migrations.
+	 */
+	public function up(): void
+	{
+		Schema::create('quotes', function (Blueprint $table) {
+			$table->id();
+			$table->json('name');
+			$table->foreignId('movie_id')->constrained()->cascadeOnDelete();
+			$table->string('picture');
+			$table->timestamps();
+		});
+	}
 
-    /**
-     * Reverse the migrations.
-     */
-    public function down(): void
-    {
-        Schema::dropIfExists('quotes');
-    }
+	/**
+	 * Reverse the migrations.
+	 */
+	public function down(): void
+	{
+		Schema::dropIfExists('quotes');
+	}
 };
