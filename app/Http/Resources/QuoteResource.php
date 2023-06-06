6bda8b45e129d62d15e5a3da6b23401e6d478bdc
data <data.shkubuliani@redberry.ge>
2023-06-07 00:33:20 +0400

    feat: add api resource for quotes

---
+++ app/Http/Resources/QuoteResource.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace App\Http\Resources;
+
+use Illuminate\Http\Request;
+use Illuminate\Http\Resources\Json\JsonResource;
+
+class QuoteResource extends JsonResource
+{
+	/**
+	 * Transform the resource into an array.
+	 *
+	 * @return array<string, mixed>
+	 */
+	public function toArray(Request $request): array
+	{
+		return [
+			'id'       => $this->id,
+			'name'     => ['en' => $this->getTranslation('name', 'en'), 'ka' => $this->getTranslation('name', 'ka')],
+			'picture'  => $this->picture,
+			'movie'    => [
+				'name'         => $this->movie->name,
+				'release_date' => $this->movie->release_date,
+			],
+			'user'         => [
+				'name'    => $this->movie->user->username,
+				'picture' => $this->movie->user->profile_picture,
+			],
+		];
+	}
+}
