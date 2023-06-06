ab8e8ef4c4becd80216266b9908aebc30fa09e91
data <data.shkubuliani@redberry.ge>
2023-06-07 00:37:30 +0400

    feat: add index, store, get, update and destroy methods in QuoteController

--- app/Http/Controllers/QuoteController.php
+++ app/Http/Controllers/QuoteController.php
@@ -2,9 +2,56 @@
 
 namespace App\Http\Controllers;
 
-use Illuminate\Http\Request;
+use App\Http\Requests\Quote\StoreQuoteRequest;
+use App\Http\Requests\Quote\UpdateQuoteRequest;
+use App\Http\Resources\QuoteResource;
+use App\Models\Quote;
+use Illuminate\Http\JsonResponse;
 
 class QuoteController extends Controller
 {
-    //
+	public function index(): JsonResponse
+	{
+		$quotes = Quote::with('movie')->latest()->paginate(3);
+
+		return response()->json(['quotes' => QuoteResource::collection($quotes), 'last_page' => $quotes->lastPage()]);
+	}
+
+	public function store(StoreQuoteRequest $request): JsonResponse
+	{
+		$this->authorize('create', [Quote::class, $request->movie_id]);
+
+		Quote::create([...$request->validated(),
+			'picture'  => request()->file('picture')->store('quotes/pictures'),
+		]);
+
+		return response()->json(['message' => 'quote created successfully'], 201);
+	}
+
+	public function get(Quote $quote): JsonResponse
+	{
+		$this->authorize('authorizeQuoteAccess', $quote);
+
+		return response()->json(['quote'  => QuoteResource::make($quote)]);
+	}
+
+	public function update(UpdateQuoteRequest $request, Quote $quote): JsonResponse
+	{
+		$this->authorize('authorizeQuoteAccess', $quote);
+
+		$quote->update([...$request->validated(),
+			'picture'  => $request->hasFile('picture') ? request()->file('picture')->store('quotes/pictures') : $quote->picture,
+		]);
+
+		return response()->json(['message' => 'quote updated successfully']);
+	}
+
+	public function destroy(Quote $quote): JsonResponse
+	{
+		$this->authorize('authorizeQuoteAccess', $quote);
+
+		$quote->delete();
+
+		return response()->json(['message' => 'Quote deleted successfully']);
+	}
 }
