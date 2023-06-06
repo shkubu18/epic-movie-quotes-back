b765102cfc39e42dd485a28704245983bbcf5b1a
data <data.shkubuliani@redberry.ge>
2023-06-07 00:38:23 +0400

    feat: add movie relation in Quote model * add guarded column * add translatable columns

--- app/Models/Quote.php
+++ app/Models/Quote.php
@@ -4,8 +4,19 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Spatie\Translatable\HasTranslations;
 
 class Quote extends Model
 {
-    use HasFactory;
+	use HasFactory, HasTranslations;
+
+	protected $guarded = ['id'];
+
+	public $translatable = ['name'];
+
+	public function movie(): BelongsTo
+	{
+		return $this->belongsTo(Movie::class);
+	}
 }
