8deb84c1693351b9db67b36a09dba8e9a5c699ae
data <data.shkubuliani@redberry.ge>
2023-06-08 18:19:19 +0400

    feat: add quote and user relation in Comment model * add guarded column

--- app/Models/Comment.php
+++ app/Models/Comment.php
@@ -4,8 +4,21 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
 
 class Comment extends Model
 {
-    use HasFactory;
+	use HasFactory;
+
+	protected $guarded = ['id'];
+
+	public function quote(): BelongsTo
+	{
+		return $this->belongsTo(Quote::class);
+	}
+
+	public function user(): BelongsTo
+	{
+		return $this->belongsTo(User::class);
+	}
 }
