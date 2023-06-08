0bafe0edc2ba5eae876543a1d8614353a619725f
data <data.shkubuliani@redberry.ge>
2023-06-08 18:15:32 +0400

    feat: add store method in CommentController

--- app/Http/Controllers/CommentController.php
+++ app/Http/Controllers/CommentController.php
@@ -2,9 +2,16 @@
 
 namespace App\Http\Controllers;
 
-use Illuminate\Http\Request;
+use App\Http\Requests\Comment\StoreCommentRequest;
+use App\Models\Comment;
+use Illuminate\Http\JsonResponse;
 
 class CommentController extends Controller
 {
-    //
+	public function store(StoreCommentRequest $request): JsonResponse
+	{
+		Comment::create($request->validated());
+
+		return response()->json(['message' => 'comment added successfully']);
+	}
 }
