0dd9c11fa803d364a89139fadb35bf464bd3d21e
data <data.shkubuliani@redberry.ge>
2023-06-20 15:16:07 +0400

    feat: add translatable textes in movies store and update form requests * update genres declaration logic

--- app/Http/Requests/Movie/UpdateMovieRequest.php
+++ app/Http/Requests/Movie/UpdateMovieRequest.php
@@ -11,16 +11,16 @@ class UpdateMovieRequest extends FormRequest
 	public function rules(): array
 	{
 		return [
-			'name_en'         => ['required', new EnglishText],
-			'name_ka'         => ['required', new GeorgianText],
+			'name_en'         => ['required', new EnglishText(__('movies.name_in_english'))],
+			'name_ka'         => ['required', new GeorgianText(__('movies.name_in_georgian'))],
 			'name'            => 'array',
 			'genres'          => 'required',
 			'release_date'    => 'required|numeric',
-			'director_en'     => ['required', new EnglishText],
-			'director_ka'     => ['required', new GeorgianText],
+			'director_en'     => ['required', new EnglishText(__('movies.director_in_english'))],
+			'director_ka'     => ['required', new GeorgianText(__('movies.director_in_georgian'))],
 			'director'        => 'array',
-			'description_en'  => ['required', new EnglishText],
-			'description_ka'  => ['required', new GeorgianText],
+			'description_en'  => ['required', new EnglishText(__('movies.description_in_english'))],
+			'description_ka'  => ['required', new GeorgianText(__('movies.description_in_georgian'))],
 			'description'     => 'array',
 			'picture'         => 'image',
 		];
@@ -29,12 +29,12 @@ class UpdateMovieRequest extends FormRequest
 	public function attributes(): array
 	{
 		return [
-			'name_en'        => 'Name in English',
-			'name_ka'        => 'Name in Georgian',
-			'director_en'    => 'Director',
-			'director_ka'    => 'Director',
-			'description_en' => 'Description',
-			'description_ka' => 'Description',
+			'name_en'        => __('movies.name_in_english'),
+			'name_ka'        => __('movies.name_in_georgian'),
+			'director_en'    => __('movies.director_in_english'),
+			'director_ka'    => __('movies.director_in_georgian'),
+			'description_en' => __('movies.description_in_english'),
+			'description_ka' => __('movies.description_in_georgian'),
 		];
 	}
 
@@ -42,7 +42,7 @@ class UpdateMovieRequest extends FormRequest
 	{
 		$this->merge([
 			'name'             => ['en' => $this->name_en, 'ka' => $this->name_ka],
-			'genres'           => explode(',', $this->genres),
+			'genres'           => $this->genres ? explode(',', $this->genres) : null,
 			'director'         => ['en' => $this->director_en, 'ka' => $this->director_ka],
 			'description'      => ['en' => $this->description_en, 'ka' => $this->description_ka],
 		]);
