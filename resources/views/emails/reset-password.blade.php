06bb75dbce08e3f8da0899e20c538f55049fa2a8
data <data.shkubuliani@redberry.ge>
2023-06-28 15:52:37 +0400

    fix: update redirect url in reset password email template

--- resources/views/emails/reset-password.blade.php
+++ resources/views/emails/reset-password.blade.php
@@ -22,11 +22,11 @@
             <p style="margin: 60px 0 0 0">Hola {{ $username }}!</p>
             <p style="margin: 25px 0">Thanks for joining Movie quotes! We really appreciate it. Please click the button below to update your password:</p>
             <br>
-            <a href="{{ env('FRONTEND_URL') . '/password/reset/' . $token }}" style="text-decoration: none; color: white; background: #E31221; padding: 10px; border-radius: 4px;">Update password</a>
+            <a href="{{ env('FRONTEND_URL') . '/?token=' . $token }}" style="text-decoration: none; color: white; background: #E31221; padding: 10px; border-radius: 4px;">Update password</a>
             <br>
             <br>
             <p style="margin: 40px 0">If clicking doesn't work, you can try copying and pasting it to your browser:</p>
-            <p style="margin: 0; word-break: break-all; color:  #DDCCAA;">{{ env('FRONTEND_URL') . '/password/reset/' . $token }}</p>
+            <p style="margin: 0; word-break: break-all; color:  #DDCCAA;">{{ env('FRONTEND_URL') . '/?token=' . $token }}</p>
             <br>
             <p style="margin: 40px 0 0 0">If you have any problems, please contact us: support@moviequotes.ge</p>
             <p style="margin: 30px 0;">MovieQuotes Crew</p>
