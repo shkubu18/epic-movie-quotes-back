460d6b14bc02fb7132f74cc81ed80bbf3351bc64
data <data.shkubuliani@redberry.ge>
2023-07-01 01:09:03 +0400

    feat: add scopeSearch method in movie and quote models

--- app/Models/Movie.php
+++ app/Models/Movie.php
@@ -2,6 +2,7 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Builder;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
@@ -16,6 +17,12 @@ class Movie extends Model
 	protected $guarded = ['id'];
 
 	public $translatable = ['name', 'director', 'description'];
+
+	public static function scopeSearch(Builder $query, ?string $search): void
+	{
+		$query->where('name->en', 'like', '%' . $search . '%')
+			->orWhere('name->ka', 'like', '%' . $search . '%');
+	}
 
 	public function user(): BelongsTo
 	{
