e6b89be9215dc972461a16e5a1528ea424bbebf7
data <data.shkubuliani@redberry.ge>
2023-07-01 01:15:44 +0400

    feat: add user api resource in getUser method of userController

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -2,12 +2,13 @@
 
 namespace App\Http\Controllers;
 
+use App\Http\Resources\UserResource;
 use Illuminate\Http\Request;
 
 class UserController extends Controller
 {
 	public function getUser(Request $request)
 	{
-		return $request->user();
+		return ['user'  => UserResource::make($request->user())];
 	}
 }
