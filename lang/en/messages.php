f8119a28a95f3f9525df016e62d685ed190d4dbe
data <data.shkubuliani@redberry.ge>
2023-07-06 17:05:48 +0400

    feat: add new translatable message

--- lang/en/messages.php
+++ lang/en/messages.php
@@ -6,4 +6,5 @@ return [
 	'failed_to_update_movie' => 'Failed to update movie. Please try again later.',
 	'movie_access'           => "You don't own this movie.",
 	'quote_access'           => "You don't own this quote.",
+	'movie_already_exists'   => 'You have already added a movie with a similar name, try adding another movie.',
 ];
