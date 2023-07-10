77a0c052aeae4b7f33ea65ba54ca3149acf18390
data <data.shkubuliani@redberry.ge>
2023-07-11 02:45:33 +0400

    feat: update NotificationController methods * update index and markAsRead method * add new method for mark all unreaded notifications as read

--- app/Http/Controllers/NotificationController.php
+++ app/Http/Controllers/NotificationController.php
@@ -5,14 +5,20 @@ namespace App\Http\Controllers;
 use App\Http\Resources\NotificationResource;
 use App\Models\Notification;
 use Illuminate\Http\JsonResponse;
+use Illuminate\Support\Facades\Auth;
 
 class NotificationController extends Controller
 {
 	public function index(): array
 	{
-		$notifications = Notification::latest()->paginate(5);
+		$notifications = Notification::where('receiver', Auth::user()->id)->orderBy('id', 'desc')->paginate(6);
+		$unreadNotificationsCount = Notification::where('receiver', Auth::user()->id)->where('read', false)->get()->count();
 
-		return ['notifications' => NotificationResource::collection($notifications), 'last_page' => $notifications->lastPage()];
+		return [
+			'notifications'              => NotificationResource::collection($notifications),
+			'last_page'                  => $notifications->lastPage(),
+			'unread_notifications_count' => $unreadNotificationsCount,
+		];
 	}
 
 	public function markAsRead(Notification $notification): JsonResponse
@@ -20,10 +26,21 @@ class NotificationController extends Controller
 		if ($notification->read === 0) {
 			$notification->update(['read' => true]);
 
-			return response()->json(['message' => 'the notification has been marked as read successfully']);
+			return response()->json(['message' => 'the notification has been marked as read successfully'], 201);
 		}
 
 		return response()->json(['message' => 'the notification is already marked as read']);
+	}
+
+	public function markAllAsRead(): JsonResponse
+	{
+		$notifications = Notification::where('receiver', Auth::user()->id)->where('read', false)->get();
+
+		foreach ($notifications as $notification) {
+			$notification->update(['read' => true]);
+		}
+
+		return response()->json(['message' => 'all notification marked as read']);
 	}
 
 	public function destroy(Notification $notification): JsonResponse
