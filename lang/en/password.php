130353f91248777138b23f6c333fdeb52a84b4f9
data <data.shkubuliani@redberry.ge>
2023-07-12 19:13:38 +0400

    feat: update password releated translatable texts

--- lang/en/password.php
+++ lang/en/password.php
@@ -1,6 +1,7 @@
 <?php
 
 return [
-	'already_sent'   => 'Reset password email is already sent.',
-	'sending_failed' => 'Failed to send reset password email.',
+	'already_sent'       => 'Reset password email is already sent.',
+	'sending_failed'     => 'Failed to send reset password email.',
+	'not_same_password'  => 'The new password must be different from the current password.',
 ];
