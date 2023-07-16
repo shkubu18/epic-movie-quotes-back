ac5cfc46e8d836005db3781336d5106b8d563e9b
data <data.shkubuliani@redberry.ge>
2023-07-16 23:42:53 +0400

    chore: update password column in users table

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -18,7 +18,7 @@ return new class() extends Migration {
 			$table->string('temporary_email')->nullable();
 			$table->string('temporary_email_verification_token')->unique()->nullable();
 			$table->timestamp('email_verified_at')->nullable();
-			$table->string('password')->nullable();
+			$table->string('password');
 			$table->string('profile_picture')->nullable();
 			$table->string('google_id')->unique()->nullable();
 			$table->rememberToken();
